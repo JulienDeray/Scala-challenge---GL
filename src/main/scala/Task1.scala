88a1b4fcb56c9a5b6c35e1e4bd0be309dda55c8a
Julien Déray <julien@deray.fr>
2017-09-13 11:18:10 +0100

    task 1 - cleans up

--- src/main/scala/Task1.scala
+++ src/main/scala/Task1.scala
@@ -1,24 +1,20 @@
-
 /**
   * @author : julienderay
   * Created on 13/09/2017
   */
-
 object Task1 {
   def lcs(left: String, right: String): String = {
-    if (left.length == 0 || right.length == 0) return ""
-
-    (left.last, right.last) match {
-      case (l, r) if l == r =>
-        lcs(allButLast(left), allButLast(right)) :+ l
-
-      case _ =>
-        (lcs(allButLast(left), right), lcs(left, allButLast(right))) match {
-          case (lChild, rChild) if lChild.length > rChild.length => lChild
-          case (_, rChild)                                       => rChild
+    def lcs: (List[Char], List[Char]) => List[Char] = {
+      case (Nil, _)                     => Nil
+      case (_, Nil)                     => Nil
+      case (l :: ls, r :: rs) if l == r => l :: lcs(ls, rs)
+      case (l :: ls, r :: rs) =>
+        (lcs(l :: ls, rs), lcs(ls, r :: rs)) match {
+          case (lChild, rChild) if lChild.size > rChild.size => lChild
+          case (_, rChild)                                   => rChild
         }
     }
-  }
 
-  private def allButLast(str: String): String = str.substring(0, str.length - 1)
+    lcs(left.toList, right.toList).mkString
+  }
 }
