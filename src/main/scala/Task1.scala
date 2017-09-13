d18187e5578b320d1b7536f73faaec708fbe25af
Julien Déray <julien@deray.fr>
2017-09-13 11:10:21 +0100

    task 1 - first impl

--- src/main/scala/Task1.scala
+++ src/main/scala/Task1.scala
@@ -5,7 +5,20 @@
   */
 
 object Task1 {
-  def  lcs(left:  String , right:  String ):  String = {
+  def lcs(left: String, right: String): String = {
+    if (left.length == 0 || right.length == 0) return ""
 
+    (left.last, right.last) match {
+      case (l, r) if l == r =>
+        lcs(allButLast(left), allButLast(right)) :+ l
+
+      case _ =>
+        (lcs(allButLast(left), right), lcs(left, allButLast(right))) match {
+          case (lChild, rChild) if lChild.length > rChild.length => lChild
+          case (_, rChild)                                       => rChild
+        }
+    }
   }
+
+  private def allButLast(str: String): String = str.substring(0, str.length - 1)
 }
--- src/test/scala/Task1Spec.scala
+++ src/test/scala/Task1Spec.scala
@@ -3,12 +3,12 @@ import org.scalatest._
 
 /**
   * @author : julienderay
-  *         Created on 08/09/2017
+  * Created on 13/09/2017
   */
 
 class Task1Spec extends FlatSpec with Matchers {
 
-  "example case" should "return ABCA" in {
-    Task1.lcs("AABACDA", "DACBBCAD") shouldBe "ABCA"
-  }
+    "the example" should "return ABCA" in {
+      Task1.lcs("AABACDA", "DACBBCAD") shouldBe "ABCA"
+    }
 }
