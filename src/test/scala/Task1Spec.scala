f99faca051ecb18dc3850bb58c99d35541a7d264
Julien Déray <julien@deray.fr>
2017-09-13 11:13:36 +0100

    task 1 - adds tests

--- src/test/scala/Task1Spec.scala
+++ src/test/scala/Task1Spec.scala
@@ -11,4 +11,24 @@ class Task1Spec extends FlatSpec with Matchers {
     "the example" should "return ABCA" in {
       Task1.lcs("AABACDA", "DACBBCAD") shouldBe "ABCA"
     }
+
+    "left empty" should "return empty" in {
+      Task1.lcs("", "DACBBCAD") shouldBe ""
+    }
+
+    "right empty" should "return empty" in {
+      Task1.lcs("AABACDA", "") shouldBe ""
+    }
+
+    "no match" should "return empty" in {
+      Task1.lcs("ABCD", "EFGH") shouldBe ""
+    }
+
+    "random 1" should "work" in {
+      Task1.lcs("AAAB", "BAB") shouldBe "AB"
+    }
+
+    "full match" should "return left / right" in {
+      Task1.lcs("ABABAB", "ABABAB") shouldBe "ABABAB"
+    }
 }
