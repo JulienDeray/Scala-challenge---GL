e187b0ff2e1fca211b0a1e9fef1b79b41e9ae0ef
Julien Déray <julien@deray.fr>
2017-09-13 11:34:59 +0100

    task 1 - adds cache

--- src/main/scala/Task1.scala
+++ src/main/scala/Task1.scala
@@ -4,12 +4,15 @@
   */
 object Task1 {
   def lcs(left: String, right: String): String = {
+    val cache = collection.mutable.Map.empty[(List[Char], List[Char]), List[Char]]
+    def getOrCompute(l: List[Char], r: List[Char]): List[Char] = cache.getOrElseUpdate((l, r), lcs(l, r))
+
     def lcs: (List[Char], List[Char]) => List[Char] = {
       case (Nil, _)                     => Nil
       case (_, Nil)                     => Nil
-      case (l :: ls, r :: rs) if l == r => l :: lcs(ls, rs)
+      case (l :: ls, r :: rs) if l == r => l :: getOrCompute(ls, rs)
       case (l :: ls, r :: rs) =>
-        (lcs(l :: ls, rs), lcs(ls, r :: rs)) match {
+        (getOrCompute(l :: ls, rs), getOrCompute(ls, r :: rs)) match {
           case (lChild, rChild) if lChild.size > rChild.size => lChild
           case (_, rChild)                                   => rChild
         }
